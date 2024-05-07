497d2a6f29dc3a4fa5c2e1ec323adf0ad3ea9961
Nathbobs <46452812+Nathbobs@users.noreply.github.com>
2024-05-07 23:35:01 +0900

    Ass 1 upload

--- Ass_1.py
+++ Ass_1.py
@@ -1,6 +1,60 @@
-# class LexicalAnalyzer():
-#     def lexer(self, inp_string):
-#         lexemes = []
-#         tokens = []
-#         curr_lexeme = ''  #current lexeme
+class SyntaxAnalyzer:
+    def lexer(self, expression):
+        lexemes = []
+        tokens = []
+        i = 0
+        n = len(expression)
+        current_lexeme = ""
 
+        while i < n:
+            char = expression[i]
+
+            if char.isdigit():
+                # Handling integers
+                current_lexeme += char
+                while i + 1 < n and expression[i + 1].isdigit():
+                    current_lexeme += expression[i + 1]
+                    i += 1
+                lexemes.append(current_lexeme)
+                tokens.append("N")
+                current_lexeme = ""
+
+            # Handling operators and parentheses
+            elif char == "+":
+                lexemes.append(char)
+                tokens.append("+")
+
+            elif char == "-":
+                lexemes.append(char)
+                tokens.append("-")
+
+            elif char == "*":
+                lexemes.append(char)
+                tokens.append("*")
+
+            elif char == "/":
+                lexemes.append(char)
+                tokens.append("/")
+
+            elif char == "(":
+                lexemes.append(char)
+                tokens.append("(")
+
+            elif char == ")":
+                lexemes.append(char)
+                tokens.append(")")
+
+            i += 1
+
+        # Append end-of-input marker
+        lexemes.append("$")
+        tokens.append("$")
+
+        return lexemes, tokens
+
+# Example usage
+S = SyntaxAnalyzer()
+
+lexemes, tokens = S.lexer("100-12/12")
+print("Lexemes: " + str(lexemes))
+print("Tokens: " + str(tokens))
